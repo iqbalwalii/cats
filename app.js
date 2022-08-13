0a30e1e845cde89de08667da3f593db57ddc570b
iqbalwalii <iqbalwalii@tuta.io>
2022-08-13 17:20:51 +0530

    initial cimmit

---
+++ app.js
@@ -0,0 +1,19 @@
+const el = document.querySelector(".hi");
+let body = document.querySelector("body");
+let image = '';
+let fact = '';
+fetch('https://catfact.ninja/fact')
+  .then((response) => response.json())
+    .then((data) => {
+        fact = data.fact;
+        console.log(fact);
+        el.innerHTML = fact;
+    });
+
+fetch('https://api.thecatapi.com/v1/images/search')
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        image = data[0].url;
+        document.body.style.backgroundImage = `url(${image})`;
+    }).catch((error) => {console.log(error) });
